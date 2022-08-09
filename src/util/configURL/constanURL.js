c4fb560a2fc2fa7627ea84f1ea3316de6a6ad4b6
siu <siuhuynh1997@gmail.com>
2022-08-09 09:18:20 +0700

    fix lich chieu phim

--- src/util/configURL/constanURL.js
+++ src/util/configURL/constanURL.js
@@ -2,4 +2,4 @@ export const DOMAIN = "https://movienew.cybersoft.edu.vn";
 
 export const TOKEN_CYBERSOFT =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAxOCIsIkhldEhhblN0cmluZyI6IjI0LzEwLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY2NjU2OTYwMDAwMCIsIm5iZiI6MTYzNzc3MzIwMCwiZXhwIjoxNjY2NzE3MjAwfQ.DWLvOaiIVlM3ruSxr9n9wTjFAhCxMgyw8spyqZIBrPE";
-export const GROUPID = "GP00";
+export const GROUPID = "GP02";
